cc9a6c5e7e04026273a239ad136d40f6fe7ee5e2
Akshay Chavan <akshaychavan.kkwedu@gmail.com>
2021-04-01 17:56:11 +0530

    Initial commit

---
+++ app/src/main/java/com/akshaychavan/mydict/fragments/DecksFragment.java
@@ -0,0 +1,41 @@
+package com.akshaychavan.mydict.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.akshaychavan.mydict.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DecksFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DecksFragment extends Fragment {
+
+
+    public DecksFragment() {
+        // Required empty public constructor
+    }
+
+    public static DecksFragment newInstance() {
+        DecksFragment fragment = new DecksFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_decks, container, false);
+    }
+}
